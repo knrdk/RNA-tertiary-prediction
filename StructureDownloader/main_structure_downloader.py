e5cbc0be17133df2cd71d180d19362c9d82905dc
knrdk <konrad1991@gmail.com>
2015-08-21 19:49:16 +0200

    Wyświetla liczbe błędów przy ściąganiu struktur

--- StructureDownloader/main_structure_downloader.py
+++ StructureDownloader/main_structure_downloader.py
@@ -2,14 +2,24 @@ __author__ = 'Konrad Kopciuch'
 from NDBResultParser import NDBResultParser
 from PDBStructureDownloader import PDBStructureDownloader
 
-if __name__ == '__main__':
+def main():
     result_directory = "C:\\RNA-structures"
     result_file_path = result_directory + "\\Result.xls"
+
     structure_ids = NDBResultParser.get_pdb_ids(result_file_path)
 
+    all_structures = 0
+    failed_structures = 0
     for id in structure_ids:
+        all_structures+=1
         try:
             PDBStructureDownloader.download_and_write(id, result_directory)
         except:
+            failed_structures+=1
             #TODO: logowanie
             print 'blad' + str(id)
+    print all_structures, failed_structures
+
+
+if __name__ == '__main__':
+    main()
