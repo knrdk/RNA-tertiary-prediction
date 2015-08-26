b6516c8a76d396570e8b5e474de596e9e472df34
knrdk <konrad1991@gmail.com>
2015-08-26 23:28:10 +0200

    odpalanie przez subprocess

--- Infernal/Infernal.py
+++ Infernal/Infernal.py
@@ -1,6 +1,12 @@
 __author__ = 'Konrad Kopciuch'
 
+import subprocess
+
 #wrapper for unix executable
 class Infernal:
     __cmscan_path = "/home/rna/infernal/cmscan"
-    __cmdatabase = "/home/rna/"
+    __cmdatabase = "/home/rna/rfam/Rfam.cm"
+
+    def scan(self):
+        input_path = "/home/rna/query.fa"
+        subprocess.check_call([Infernal.__cmscan_path, Infernal.__cmdatabase, input_path])
