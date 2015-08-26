cc55dd096b10872532ea25d507ea2334637b69bd
knrdk <konrad1991@gmail.com>
2015-08-26 14:35:28 -0700

    poprawne uruchamianie "cmscan"

--- Infernal/Infernal.py
+++ Infernal/Infernal.py
@@ -7,6 +7,12 @@ class Infernal:
     __cmscan_path = "/home/rna/infernal/cmscan"
     __cmdatabase = "/home/rna/rfam/Rfam.cm"
 
+
+    def __init__(self):
+        pass
+
     def scan(self):
-        input_path = "/home/rna/query.fa"
-        subprocess.check_call([Infernal.__cmscan_path, Infernal.__cmdatabase, input_path])
+        input_path = "/home/rna/query/subject.fa"
+        proc = subprocess.Popen([Infernal.__cmscan_path, Infernal.__cmdatabase, input_path], stdout=subprocess.PIPE)
+        output = proc.stdout.read()
+        print output
---
+++ Infernal/main_infernal.py
@@ -0,0 +1,11 @@
+__author__ = 'rna'
+
+from Infernal import Infernal
+
+def main():
+    inf = Infernal()
+    inf.scan()
+
+
+if __name__ == "__main__":
+    main()
