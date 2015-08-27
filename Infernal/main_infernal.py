c4e31232c1d076e5c59a9f057c6031f337c57c56
knrdk <konrad1991@gmail.com>
2015-08-27 00:15:20 -0700

    dziaajace cmscan

--- Infernal/main_infernal.py
+++ Infernal/main_infernal.py
@@ -1,13 +1,16 @@
 __author__ = 'rna'
 
 from Infernal import Infernal
+from FamilyFileParser import FamilyFileParser
+from CmScanOutputParser import CmScanOutputParser
 
 def main():
     inf = Infernal()
     output = inf.scan()
 
-    with open("/home/rna/output.txt",'w') as file:
-        file.write(output)
+    parser = CmScanOutputParser(output)
+    families = parser.get_families_above_threshold()
+    print families
 
 
 if __name__ == "__main__":
