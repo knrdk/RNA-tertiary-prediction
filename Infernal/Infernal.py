8aac524f3ef70cda95e67b406a101e7315d4f8cf
knrdk <konrad1991@gmail.com>
2015-08-27 15:25:31 +0200

    konfiguracja wydzielona do pliku

--- Infernal/Infernal.py
+++ Infernal/Infernal.py
@@ -5,29 +5,26 @@ import os
 
 from CmScanOutputParser import CmScanOutputParser
 
-#wrapper for unix executable
-class Infernal:
-    __cmscan_path = "/home/rna/infernal/cmscan"
-    __cmdatabase = "/home/rna/rfam/Rfam.cm"
-    __tmp_file_path = "/home/rna/RNA/temp_get_families_for_sequence.fa"
 
-    def __init__(self):
-        #TODO: read configuration
-        pass
+class Infernal:
+    def __init__(self, config):
+        self.config = config
 
     def get_families_for_sequence(self, sequence):
-        with open(Infernal.__tmp_file_path,'w+') as f:
+        temp_file_path = self.config['tempscanfilepath']
+        with open(temp_file_path,'w+') as f:
             f.write(">sequence")
             f.write(sequence)
-        output = Infernal.__scan(Infernal.__tmp_file_path)
+        output = Infernal.__scan(temp_file_path)
         parser = CmScanOutputParser(output)
         families = parser.get_families_above_threshold()
-        Infernal.__delete_file_if_exist(sequence)
+        Infernal.__delete_file_if_exist(temp_file_path)
         return families
 
-    @staticmethod
-    def __scan(input_path):
-        proc = subprocess.Popen([Infernal.__cmscan_path, Infernal.__cmdatabase, input_path], stdout=subprocess.PIPE)
+    def __scan(self, input_path):
+        cmscan_path = self.config['cmscan']
+        cmdatabase_path = self.config['cmdatabase']
+        proc = subprocess.Popen([cmscan_path, cmdatabase_path, input_path], stdout=subprocess.PIPE)
         output = proc.stdout.read()
         return output
 
--- Infernal/main_infernal.py
+++ Infernal/main_infernal.py
@@ -1,13 +1,27 @@
 __author__ = 'rna'
 
 from Infernal import Infernal
-from FamilyFileParser import FamilyFileParser
+from ConfigParser import ConfigParser
+
+
+def get_config(path):
+    config = ConfigParser()
+    config.read(path)
+    section = 'Infernal'
+    options = config.options(section)
+    dict1 = dict()
+    for option in options:
+        try:
+            dict1[option] = config.get(section, option)
+        except:
+            dict1[option] = None
+    return dict1
 
 def main():
-    inf = Infernal()
+    config = get_config("config.ini")
+    inf = Infernal(config)
     families = inf.get_families_for_sequence("GCGUCG")
     print families
-
 
 if __name__ == "__main__":
     main()
