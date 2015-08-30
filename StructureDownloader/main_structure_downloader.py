c1dd73441ed3d32bd987649d6cb4c014e463a4b4
knrdk <konrad1991@gmail.com>
2015-08-30 11:19:43 -0700

    dodane logowanie + wczytywanie parametrow z linii komend

--- StructureDownloader/main_structure_downloader.py
+++ StructureDownloader/main_structure_downloader.py
@@ -1,25 +1,39 @@
 __author__ = 'Konrad Kopciuch'
+import logging
+import sys
+from datetime import datetime
+
 from NDBResultParser import NDBResultParser
 from PDBStructureDownloader import PDBStructureDownloader
 
-def main():
-    result_directory = "/home/rna/RNA/Structures"
-    result_file_path = "/home/rna/RNA/Result.xls"
 
-    structure_ids = NDBResultParser.get_pdb_ids(result_file_path)
+def __get_logger():
+    logger = logging.getLogger()
+    fh = logging.FileHandler('log.txt')
+    logger.addHandler(fh)
+    logger.setLevel(logging.INFO)
+    return logger
 
-    all_structures = 0
-    failed_structures = 0
-    for id in structure_ids:
+def main(result_directory, input_file_path):
+    loggger = __get_logger()
+    loggger.info("Downloading started: %s", datetime.now())
+
+    all_structures, failed_structures = 0, 0
+    for id in NDBResultParser.get_pdb_ids(input_file_path):
         all_structures+=1
         try:
             PDBStructureDownloader.download_and_write(id, result_directory)
         except:
             failed_structures+=1
-            #TODO: logowanie
-            print 'blad' + str(id)
-    print all_structures, failed_structures
+            loggger.error("ERROR %s", str(id))
+    downloaded_structure = all_structures - failed_structures
+    print 'liczba sciagnietych plikow pdb: ', downloaded_structure
+    print 'liczba plikow przy pobieraniu ktorych wystapily bledy: ', failed_structures
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) < 3:
+        print "main_structure_downloader.py result_directory input_file_path"
+    result_directory = sys.argv[1]
+    input_file_path = sys.argv[2]
+    main(result_directory, input_file_path)
