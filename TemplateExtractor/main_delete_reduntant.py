6be6eaaf08a161c0a717fd6a1774309a3092ccad
knrdk <konrad1991@gmail.com>
2015-09-01 02:37:49 -0700

    wypisywanie usuwanych szablonow

--- TemplateExtractor/main_delete_reduntant.py
+++ TemplateExtractor/main_delete_reduntant.py
@@ -9,8 +9,9 @@ def delete_redundant():
     groupped = [[(y[0],y[2]) for y in all_sequences if y[1] == x] for x in distinct_sequences]
 
     for group in groupped:
-        s = sorted(group, key=lambda x: x[1])
+        s = sorted(group, key=lambda x: x[1]) #sortowanie po rozdzielczosci
         for x in s[1:]:
+            print "usuwanie szablonu" + x[0]
             structure_id, chain_id = x[0].split('_')
             repo.delete_template(structure_id, chain_id)
 
