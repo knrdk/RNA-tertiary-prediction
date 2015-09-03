8e22f595ad07e3d61ac618835844f95461498adf
knrdk <konrad1991@gmail.com>
2015-09-03 22:41:27 +0200

    ścieżka do macierzy ribosum w configu

--- SCFG/main_SCFG_test.py
+++ SCFG/main_SCFG_test.py
@@ -1,7 +1,11 @@
 __author__ = 'Konrad Kopciuch'
 
-from SecondaryStructureToSCFGParser import SecondaryStructureToSCFGParser
+from ConfigParser import ConfigParser
 
+from SecondaryStructureToSCFGParser import SecondaryStructureToSCFGParser
+from RibosumMatrix.RibosumParser import RibosumParser
+
+'''
 ss, sq = "..(((....).)).((.(...)))", "ACGUACGUACGUACGUACGUACGU"
 #ss, sq = ".()()", "AGCAU"
 #ss, sq = ".()", "AGC"
@@ -9,3 +13,19 @@ ss, sq = "..(((....).)).((.(...)))", "ACGUACGUACGUACGUACGUACGU"
 
 parser = SecondaryStructureToSCFGParser()
 parser.get_SCFG(ss, sq)
+'''
+
+def get_ribosum_matrix_path(config_path):
+    config = ConfigParser()
+    config.read(config_path)
+    section = 'Ribosum'
+    matrix_path = "matrixpath"
+    return config.get(section, matrix_path)
+
+ribosum_matrix_path = get_ribosum_matrix_path("config.ini")
+parser = RibosumParser(ribosum_matrix_path)
+sm = parser.get_single_matrix()
+dm = parser.get_double_matrix()
+
+print sm
+print dm
