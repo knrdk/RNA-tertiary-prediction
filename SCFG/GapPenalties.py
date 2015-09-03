8d3313a593c701ef0b459c5c1a57bd87446b554c
knrdk <konrad1991@gmail.com>
2015-09-03 22:08:51 +0200

    dodanie gap penalties

---
+++ SCFG/GapPenalties.py
@@ -0,0 +1,60 @@
+__author__ = 'Konrad Kopciuch'
+
+from GapClasses import *
+
+def get_penalties_dictionary(a, b):
+    """
+    Zwraca macierz zawierajaca kary za przerwy w dopasowaniu sekewncji
+    :param a: parametr alfa z publikacji "RSEARCH: Finding homologs of single structured RNA sequences" Klein, Eddy
+    :param b: parametr beta z tej publikacji
+    :return: s³ownik dwuwymiarowy indeksowany klasami kar za otwarcie. Pierwszy argument From_class, drugi To_class
+    """
+    return {
+        M_cl: {
+            M_cl: 0,
+            IL_cl: 0.5*a,
+            DL_cl: 0.5*a,
+            IR_cl: 0.5*a,
+            DR_cl: 0.5*a,
+            DB_cl: 0.5*a
+        },
+        IL_cl: {
+            M_cl: b + 0.5*a,
+            IL_cl: b,
+            DL_cl: b+a,
+            IR_cl: b+a,
+            DR_cl: b+a,
+            DB_cl: b+1.5*a
+        },
+        DL_cl: {
+            M_cl: b + 0.5*a,
+            IL_cl: b + a,
+            DL_cl: b,
+            IR_cl: b + a,
+            DR_cl: b + a,
+            DB_cl: b + 0.5*a
+        },
+        IR_cl: {
+            M_cl: b + 0.5*a,
+            DL_cl: b + a,
+            IR_cl: b,
+            DR_cl: b + a,
+            DB_cl: b + 1.5*a
+        },
+        DR_cl: {
+            M_cl: b + 0.5*a,
+            IL_cl: b + a,
+            DL_cl: b + a,
+            IR_cl: b + a,
+            DR_cl: b,
+            DB_cl: b + 1.5*a
+        },
+        DB_cl: {
+            M_cl: 2*b + a,
+            IL_cl: 2*b + 1.5*a,
+            DL_cl: 2*b + 0.5*a,
+            IR_cl: 2*b + 1.5*a,
+            DR_cl: 2*b + 0.5*a,
+            DB_cl: 2*b
+        }
+    }
--- SCFG/SCFG.py
+++ SCFG/SCFG.py
@@ -1,7 +1,10 @@
 __author__ = 'Konrad Kopciuch'
 
+from GapPenalties import get_penalties_dictionary
+
 class SCFG:
 
     def __init__(self, states, single_scoring_matrix, double_scoring_matrix):
         self.states = []
+        self.gap_penalties = get_penalties_dictionary(1,2) #TODO: prawdziwe wartosci
 
