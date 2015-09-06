1e15cb552eafdbc026684da5bfa5ce7cb2f7efd2
knrdk <konrad1991@gmail.com>
2015-09-06 06:03:55 -0700

    dodanie macierzy TAU

--- SCFG/SCFG.py
+++ SCFG/SCFG.py
@@ -11,14 +11,17 @@ class SCFG:
         self.single_matrix = single_scoring_matrix
         self.double_matrix = double_scoring_matrix
 
-    def get_score(self, sequence):
-        self.sequence = sequence
-        n_states = len(self.states)
-        n_sequence = len(sequence)
-        self.alpha = self.__get_3D_array(n_states, n_sequence+1, n_sequence+1)
-        self.__CYK_Inside(0, n_states-1, 0, n_sequence-1)
-        score = self.alpha[0][0][n_sequence - 1]
+    def get_score(self):
+        score = self.alpha[0][0][len(self.sequence) - 1]
         return score
+
+    def align(self, sequence):
+        self.sequence = sequence
+        states_length = len(self.states)
+        sequence_length = len(sequence)
+        self.alpha = self.__get_3D_array(states_length, sequence_length+1, sequence_length+1)
+        self.tau = self.__get_3D_array(states_length, sequence_length+1, sequence_length+1)
+        self.__CYK_Inside(0, states_length-1, 0, sequence_length-1)
 
     def get_transition_cost(self, begin_state, end_state):
         begin_class = begin_state.get_gap_class()
@@ -47,60 +50,57 @@ class SCFG:
                     d = j-i+1
                     state = self.states[v]
                     state_type = state.state_type
+                    maximum, index_max = float("-inf"), -1
+
                     if state_type in {StateTypes.D, StateTypes.S}:
-                        max = float("-inf")
+                        maximum, index_max = float("-inf"), -1
                         for gamma in state.get_connected_indexes():
                             gamma_value = self.alpha[gamma][i][j]
                             transition_cost = self.get_transition_cost(state, self.states[gamma])
                             value = gamma_value + transition_cost
-                            if value > max:
-                                max = value
-                        self.alpha[v][i][j] = max
+                            if value > maximum:
+                                maximum, index_max = value, gamma
                     elif state_type == StateTypes.P and d>=2:
-                        base_pair = self.sequence[i] + self.sequence[j]
-                        emission_cost = self.get_double_emission_cost(state, base_pair)
-                        max = float("-inf")
                         for gamma in state.get_connected_indexes():
                             gamma_value = self.alpha[gamma][i+1][j-1]
                             transition_cost = self.get_transition_cost(state, self.states[gamma])
                             value = gamma_value + transition_cost
-                            if value > max:
-                                max = value
-                        self.alpha[v][i][j] = max + emission_cost
+                            if value > maximum:
+                                maximum, index_max = value, gamma
+                        base_pair = self.sequence[i] + self.sequence[j]
+                        emission_cost = self.get_double_emission_cost(state, base_pair)
+                        maximum = maximum + emission_cost
                     elif state_type == StateTypes.L and d>=1:
-                        emission_cost = self.get_single_emission_cost(state, self.sequence[i])
-                        max = float("-inf")
                         for gamma in state.get_connected_indexes():
                             gamma_value = self.alpha[gamma][i+1][j]
                             transition_cost = self.get_transition_cost(state, self.states[gamma])
                             value = gamma_value + transition_cost
-                            if value > max:
-                                max = value
-                        self.alpha[v][i][j] = max + emission_cost
+                            if value > maximum:
+                                maximum, index_max = value, gamma
+                        emission_cost = self.get_single_emission_cost(state, self.sequence[i])
+                        maximum = maximum + emission_cost
                     elif state_type == StateTypes.R and d>=1:
-                        emission_cost = self.get_single_emission_cost(state, self.sequence[j])
-                        max = float("-inf")
                         for gamma in state.get_connected_indexes():
                             gamma_value = self.alpha[gamma][i][j-1]
                             transition_cost = self.get_transition_cost(state, self.states[gamma])
                             value = gamma_value + transition_cost
-                            if value > max:
-                                max = value
-                        self.alpha[v][i][j] = max + emission_cost
+                            if value > maximum:
+                                maximum, index_max = value, gamma
+                        emission_cost = self.get_single_emission_cost(state, self.sequence[j])
+                        maximum = maximum + emission_cost
                     elif state_type == StateTypes.B:
                         left, right = state.get_connected_indexes()
-                        max = float("-inf")
                         for k in range(i,j,1):
                             left_value = self.alpha[left][i][k]
                             right_value = self.alpha[right][k+1][j]
                             value = left_value + right_value
-                            if value > max:
-                                max = value
-                        self.alpha[v][i][j] = max
+                            if value > maximum:
+                                maximum, index_max = value, k
                     elif state_type == StateTypes.E and d == 0:
-                        self.alpha[v][i][j] = 0
-                    else:
-                        self.alpha[v][i][j] = float("-inf")
+                        maximum = 0
+
+                    self.alpha[v][i][j] = maximum
+                    self.tau[v][i][j] = index_max
 
     def __get_3D_array(self, depth, width, height):
         return [self.__get_2D_array(width, height) for x in range(depth)]
