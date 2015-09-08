98c973658659b674a880d40f7b5cf0a07ac32356
knrdk <konrad1991@gmail.com>
2015-09-08 15:39:17 +0200

    .

--- RMSD/PredictionSignificance.py
+++ RMSD/PredictionSignificance.py
@@ -1,4 +1,28 @@
 __author__ = 'rna'
 
-def get_pvalue(chain_length, rmsd):
-    pass
+from math import sqrt, pow, erf
+
+def __get_pvalue_without_base_pairing(chain_length, rmsd):
+    std = 1.8
+    a = 4.6 * sqrt(2.0)
+    b = 9.1 * sqrt(2.0)
+    average = a * pow(chain_length, 0.41) - b
+    z = (rmsd - average)/std
+    pvalue = (1 + erf(z/sqrt(2)))/2.0
+    return pvalue
+
+def __get_pvalue_with_base_pairing(chain_length, rmsd):
+    std = 1.8
+    a = 3.6 * sqrt(2.0)
+    b = 11.2 * sqrt(2.0)
+    average = a * pow(chain_length, 0.41) - b
+    z = (rmsd - average)/std
+    pvalue = (1 + erf(z/sqrt(2)))/2.0
+    return pvalue
+
+def get_pvalue(chain_length, rmsd, base_pairing=True):
+    if base_pairing:
+        return __get_pvalue_with_base_pairing(chain_length, rmsd)
+    else:
+        return __get_pvalue_without_base_pairing(chain_length, rmsd)
+
---
+++ RMSD/main_test.py
@@ -0,0 +1,5 @@
+__author__ = 'Konrad Kopciuch'
+
+from PredictionSignificance import get_pvalue
+
+print get_pvalue(71, 1.7, True)
