f367f99102edecbff92f1f6906ed1d0c8eeade5d
knrdk <konrad1991@gmail.com>
2015-09-09 14:29:40 -0700

    cross validation wielowatkowe

--- Training/main_cross.py
+++ Training/main_cross.py
@@ -10,7 +10,18 @@ from SCFG.SecondaryStructureToSCFGParser import SecondaryStructureToSCFGParser
 from SCFG.ScoringMatrix import get_scoring_matrices
 from Utils.Alignment import *
 from RMSD.RMSD import get_rmsd
-from SequenceProvider import SequenceProvider
+from os import remove
+from multiprocessing import Pool, cpu_count
+from functools import partial
+
+
+def __get_thread_pool():
+    try:
+        cpus = cpu_count()
+    except NotImplementedError:
+        cpus = 1   # arbitrary default
+
+    return Pool(processes=cpus)
 
 def __nw_align(template_sequence, query_sequence):
     nw = NeedlemanWunsch(template_sequence, query_sequence)
@@ -18,22 +29,22 @@ def __nw_align(template_sequence, query_sequence):
     return nw.get_alignment()
 
 def __scfg_align(template_secstruct, template_sequence, query_sequence):
-    single_matrix, double_matrix = get_scoring_matrices('config.ini')
+    single_matrix, double_matrix = get_scoring_matrices('./../config.ini')
     parser = SecondaryStructureToSCFGParser(single_matrix, double_matrix)
     scfg = parser.get_SCFG(template_secstruct, template_sequence)
     scfg.align(query_sequence)
     algn = scfg.get_alignment()
     return algn
 
-def get_alignment(template_sequence_provider, query_sequence_provider, template_path, query_path):
-    query_sequence = query_sequence_provider.get_sequence(query_path)
-    query_sequence_without_modifications = query_sequence_provider.get_unmodified_sequence(query_path)
-    template_sequence = template_sequence_provider.get_sequence(template_path)
-    template_sequence_without_modifications = template_sequence_provider.get_unmodified_sequence(template_path)
-    template_secondary_structure = template_sequence_provider.get_secondary_structure(template_path)
+def get_alignment(template_path,
+                  query_path,
+                  query_sequence,
+                  query_sequence_without_modifications,
+                  template_sequence,
+                  template_sequence_without_modifications,
+                  template_secondary_structure):
 
-    #algn = __scfg_align(template_secondary_structure, template_sequence_without_modifications, query_sequence_without_modifications)
-    algn = __nw_align(template_sequence_without_modifications, query_sequence_without_modifications)
+    algn = __scfg_align(template_secondary_structure, template_sequence_without_modifications, query_sequence_without_modifications)
 
     algn.change_template_sequence(template_sequence)
     algn.change_query_sequence(query_sequence)
@@ -41,50 +52,92 @@ def get_alignment(template_sequence_provider, query_sequence_provider, template_
     algn.set_query_description("Query")
     return algn
 
+def process_pair(templates_directory,
+                 training_templates_directory,
+                  sinfo):
+    (template_path,
+     query_path,
+     query_sequence,
+     query_sequence_without_modifications,
+     template_sequence,
+     template_sequence_without_modifications,
+     template_secondary_structure) = sinfo
+    algn = get_alignment(template_path, query_path, query_sequence, query_sequence_without_modifications, template_sequence,
+                         template_sequence_without_modifications, template_secondary_structure)
+    tmp_filename = query_path.split('.')[0] + template_path.split('.')[0]
+    write_alignment(algn, tmp_filename + '.fasta')
+    try:
+        a = load_alignment(tmp_filename + '.fasta')
+        template_full_path = templates_directory + template_path
+        t = load_template(template_full_path)
+        m = create_model(t,a)
+        m.write_pdb_file(tmp_filename + '.pdb')
+
+        query_full_path = training_templates_directory + query_path
+        rmsd = get_rmsd(query_full_path, tmp_filename + '.pdb')
+    except:
+        rmsd = float("inf")
+    finally:
+        remove(tmp_filename + '.fasta')
+        remove(tmp_filename + '.pdb')
+    print query_path, template_path, rmsd
+    return (query_path, template_path, rmsd)
+
+def load_sequences(templates_directory, template_path):
+    full_path = templates_directory + template_path
+    tmpl = load_template(full_path)
+    sequence = tmpl.get_sequence()
+    unmodified_sequence = str(sequence.seq_without_modifications)
+    secondary_structure = str(tmpl.get_secstruc())
+    return str(sequence), unmodified_sequence, secondary_structure
+
+def get_sequences(directory, paths):
+    func = partial(load_sequences, directory)
+    pool = __get_thread_pool()
+    q = pool.map(func, paths)
+
+    output = dict()
+    for (index, path) in enumerate(paths):
+        output[path] = q[index]
+    return output
+
 def main():
-    cfg = Config('config.ini')
+    cfg = Config('./../config.ini')
     templates_directory = cfg.get_template_directory()
     training_templates_directory = cfg.get_training_set_directory()
     training_results_file = cfg.get_training_results_path()
-    templates_sequence_provider = SequenceProvider(templates_directory)
-    training_sequence_provider = SequenceProvider(training_templates_directory)
 
     templates_paths = list(listdir(templates_directory))
     training_paths = list(listdir(training_templates_directory))
 
     print "Liczba szablonow: ", len(templates_paths)
     print "Liczba elementow zbioru treningowego: ", len(training_paths)
-    print "Liczba par: ", str(len(templates_paths)*len(training_paths))
 
-    i = 1
-    with open(training_results_file, 'w') as f:
-        for query_path in training_paths:
+    templates_sequences = get_sequences(templates_directory, templates_paths)
+    training_sequences = get_sequences(training_templates_directory, training_paths)
+
+    print "dane o sekwencji zaladowane"
+
+    data = list()
+    for query_path in training_paths:
             for template_path in templates_paths:
                 if query_path == template_path:
                     continue
-                print i
-                print query_path, template_path
+                (q_seq, q_unm, q_ss) = training_sequences[query_path]
+                (t_seq, t_unm, t_ss) = templates_sequences[template_path]
+                record = (template_path, query_path, q_seq, q_unm, t_seq, t_unm, t_ss)
+            data.append(record)
 
-                algn = get_alignment(templates_sequence_provider, training_sequence_provider, template_path, query_path)
-                write_alignment(algn, 'temp.fasta')
+    func = partial(process_pair, templates_directory, training_templates_directory)
+    pool = __get_thread_pool()
+    results = pool.map(func, data)
 
-                try:
-                    a = load_alignment('temp.fasta')
-                    template_full_path = templates_directory + template_path
-                    t = load_template(template_full_path)
-                    m = create_model(t,a)
-                    m.write_pdb_file('temp.pdb')
-
-                    query_full_path = training_templates_directory + query_path
-                    rmsd = get_rmsd(query_full_path, 'temp.pdb')
-                except:
-                    rmsd = float("inf")
-
-                print rmsd
-                i+=1
-                f.write(query_path + '\t' + template_path + '\t' + str(rmsd) + '\n')
-                f.flush()
+    with open(training_results_file, 'w') as f:
+        for (query_path, template_path, rmsd) in results:
+            f.write(query_path + '\t' + template_path + '\t' + str(rmsd) + '\n')
+            f.flush()
 
 
-main()
+if __name__ == '__main__':
+    main()
 
