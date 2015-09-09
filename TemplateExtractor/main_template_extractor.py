1f217bea5b7e1e54f7115738d247e2bb17bc75a1
knrdk <konrad1991@gmail.com>
2015-09-09 06:24:07 -0700

    multiprocessing

--- TemplateExtractor/main_template_extractor.py
+++ TemplateExtractor/main_template_extractor.py
@@ -8,15 +8,10 @@ from Utils import Template
 from TemplateExtractorLogger import TemplateExtractorLogger
 from Repository import MongoTemplateRepository
 from Config import Config
+import multiprocessing
 
-
-def main_template_extractor(structures_directory, templates_directory):
-    logger = TemplateExtractorLogger()
-    logger.log_start(structures_directory, templates_directory)
-    writer = TemplateWriter(templates_directory)
-    repo = MongoTemplateRepository.MongoTemplateRepository()
-    for file_path in os.listdir(structures_directory):
-        if file_path.endswith(".ent") or file_path.endswith(".pdb"):
+def process_structure_file(file_path, repo, writer, logger):
+    if file_path.endswith(".ent") or file_path.endswith(".pdb"):
             print 'Wyodrebnianie szablonow z pliku: ', file_path
             logger.log_filename(file_path)
             structure_path = structures_directory + file_path
@@ -30,6 +25,24 @@ def main_template_extractor(structures_directory, templates_directory):
                 writer.write(template)
                 logger.log_template_extracted(id, tinfo)
                 print tinfo.id, tinfo.chain_id
+
+def main_template_extractor(structures_directory, templates_directory):
+    logger = TemplateExtractorLogger()
+    logger.log_start(structures_directory, templates_directory)
+    writer = TemplateWriter(templates_directory)
+    repo = MongoTemplateRepository.MongoTemplateRepository()
+    structures_path = list()
+    for file_path in os.listdir(structures_directory):
+        structures_path.append((file_path, repo, writer, logger))
+
+    try:
+        cpus = multiprocessing.cpu_count()
+    except NotImplementedError:
+        cpus = 2   # arbitrary default
+
+    pool = multiprocessing.Pool(processes=cpus)
+    pool.map(process_structure_file, structures_path)
+
 
 
 if __name__ == '__main__':
