9b03fa599e7432645e9ef8aab8a2e740fa43d075
knrdk <konrad1991@gmail.com>
2015-09-11 03:10:58 -0700

    refaktoryzacja

---
+++ Training/TrainingDataProvider.py
@@ -0,0 +1,53 @@
+__author__ = 'Konrad Kopciuch'
+
+from Repository.MongoTrainingTemplateRepository import MongoTrainingTemplateRepository
+from RMSD.PredictionSignificance import get_pvalue
+import Utils.FloatList as fl
+
+
+def get_template_id_from_filename(filename):
+        return filename.split('.')[0]
+
+
+def get_trainings_chains_lenght():
+    repo = MongoTrainingTemplateRepository()
+    return repo.get_chains_lengths()
+
+
+def get_folds_dict(file_with_rmsd):
+    chains_lengths = get_trainings_chains_lenght()
+
+    output = dict()
+    with open(file_with_rmsd, 'r') as f:
+        for line in f:
+            (query, template, rmsd) = line.split('\t')
+            query_id = get_template_id_from_filename(query) #TODO: w pliku zapisywac juz id a nie filename
+            template_id = get_template_id_from_filename(template)
+
+            pvalue = get_pvalue(chains_lengths[query_id], float(rmsd))
+            is_same_fold = pvalue < 0.01
+
+            if not output.has_key(query_id):
+                output[query_id] = dict()
+            output[query_id][template_id] = is_same_fold
+    return output
+
+
+def get_train_data(file_with_rmsd, file_with_feature_vector):
+    folds_dict = get_folds_dict(file_with_rmsd)
+
+    data = []
+    target = []
+
+    with open(file_with_feature_vector, 'r') as f:
+        for line in f:
+            (query, template, fv) = line.split('\t')
+
+            query_id = get_template_id_from_filename(query)
+            template_id = get_template_id_from_filename(template)
+
+            same_fold = folds_dict[query_id][template_id]
+            data.append(fl.parse_from_string(fv))
+            target.append(same_fold)
+
+    return data, target
--- main_svm_cross_validation.py
+++ main_svm_cross_validation.py
@@ -1,62 +1,18 @@
 __author__ = 'Konrad Kopciuch'
 
-from Repository.MongoTrainingTemplateRepository import MongoTrainingTemplateRepository
-from RMSD.PredictionSignificance import get_pvalue
+
 from Config import Config
-import Utils.FloatList as fl
+from Training.TrainingDataProvider import get_train_data
 from sklearn import svm
 from sklearn import cross_validation
 
 
-def get_template_id_from_filename(filename):
-    return filename.split('.')[0]
-
-
-def get_trainings_chains_lenght():
-    repo = MongoTrainingTemplateRepository()
-    return repo.get_chains_lengths()
-
-
-def get_folds_dict(file_with_rmsd):
-    chains_lengths = get_trainings_chains_lenght()
-
-    output = dict()
-    with open(file_with_rmsd, 'r') as f:
-        for line in f:
-            (query, template, rmsd) = line.split('\t')
-            query_id = get_template_id_from_filename(query) #TODO: w pliku zapisywac juz id a nie filename
-            template_id = get_template_id_from_filename(template)
-
-            pvalue = get_pvalue(chains_lengths[query_id], float(rmsd))
-            is_same_fold = pvalue < 0.01
-
-            if not output.has_key(query_id):
-                output[query_id] = dict()
-            output[query_id][template_id] = is_same_fold
-    return output
-
-
 def main_cross_validation():
     cfg = Config('config.ini')
-    models_rmsd_file = cfg.get_training_results_path()
+    file_with_rmsd = cfg.get_training_results_path()
     feature_vectors_file = cfg.get_feature_vectors_path()
 
-    folds_dict = get_folds_dict(models_rmsd_file)
-
-    data = []
-    target = []
-
-    with open(feature_vectors_file, 'r') as f:
-        for line in f:
-            (query, template, fv) = line.split('\t')
-
-            query_id = get_template_id_from_filename(query)
-            template_id = get_template_id_from_filename(template)
-
-            same_fold = folds_dict[query_id][template_id]
-            data.append(fl.parse_from_string(fv))
-            target.append(same_fold)
-
+    data, target = get_train_data(file_with_rmsd, feature_vectors_file)
     data_train, data_test, target_train, target_test = cross_validation.train_test_split(
         data, target, test_size=0.2, random_state=0)
 
