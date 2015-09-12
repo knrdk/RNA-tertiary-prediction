f33a59011d1ae467ff9098ec5adc34423217713c
knrdk <konrad1991@gmail.com>
2015-09-12 14:48:20 -0700

    refaktoryzacja

--- main_training_delete_redundant.py
+++ main_training_delete_redundant.py
@@ -6,8 +6,29 @@ from Repository.MongoTrainingTemplateRepository import MongoTrainingTemplateRepo
 from Config import Config
 
 
-def delete_template(repository, db_id):
+def __delete_template(repository, db_id):
+    '''
+    Funckja usuwa z bazy danych informacje o szablonie
+    :param repository: obiekt klasy MongoTrainingTemplateRepository
+    :param db_id: identyfikator w bazie danych szablonu do usuniecia
+    :return:
+    '''
     repository.delete_template(db_id)
+
+def __delete_unused_template_files(repository, template_directory):
+    '''
+    Funkcja usuwa pliki szablonow o ktorych informacji nie ma w bazie danych
+    :param repository: obiekt klasy MongoTrainingTemplateRepository
+    :param template_directory: sciezka do folderu z szablonami zbioru treningowego
+    :return: Funkcja nic nie zwraca
+    '''
+    templates_id = set(repository.get_all_templates_id())
+    for file_path in listdir(template_directory):
+        file_id = file_path.split('.')[0].upper()
+        if not file_id in templates_id:
+            full_path = path.join(template_directory, file_path)
+            remove(full_path)
+            print "usuwanie pliku szablonu: ", file_path
 
 def main_delete_redundant(template_directory):
     repo = MongoTrainingTemplateRepository()
@@ -21,15 +42,9 @@ def main_delete_redundant(template_directory):
 
     for db_id in templates_to_delete:
         print "usuwanie szablonu: " + str(db_id)
-        delete_template(repo, db_id)
+        __delete_template(repo, db_id)
 
-    templates_id = set(repo.get_all_templates_id())
-    for file_path in listdir(template_directory):
-        file_id = file_path.split('.')[0].upper()
-        if not file_id in templates_id:
-            full_path = path.join(template_directory, file_path)
-            remove(full_path)
-            print "usuwanie pliku: ", file_path
+    __delete_unused_template_files(repo, template_directory)
 
 
 if __name__ == "__main__":
