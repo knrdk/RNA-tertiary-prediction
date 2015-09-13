aebeb983bc93e0ee6417c6ed3f44c19813881de0
knrdk <konrad1991@gmail.com>
2015-09-13 00:40:22 -0700

    .

--- PairwiseSimilarity/FeatureVectorCalculator.py
+++ PairwiseSimilarity/FeatureVectorCalculator.py
@@ -9,14 +9,14 @@ from DifferenceInLenght import DifferenceInLenght
 from InfernalBestFamilyScore import InfernalBestFamilyScore
 from SCFGScore import SCFGScore
 
-methods = [AffiniteNeedlemanWunschIdentity,
-           AffiniteNeedlemanWunschScore,
-           LinearNeedlemanWunschIdentity,
-           Ribosum85NeedlemanWunschIdentity,
-           Ribosum85NeedlemanWunschScore,
-           DifferenceInLenght]
+methods = [#AffiniteNeedlemanWunschIdentity,
+           #AffiniteNeedlemanWunschScore,
+           #LinearNeedlemanWunschIdentity,
+           #Ribosum85NeedlemanWunschIdentity,
+           #Ribosum85NeedlemanWunschScore,
+           #DifferenceInLenght,
            #InfernalBestFamilyScore,
-           #SCFGScore]
+           SCFGScore]
 
 class FeatureVectorCalculator:
 
