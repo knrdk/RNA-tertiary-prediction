668dab6b30398c458e39cf4091d32d321c10175b
knrdk <konrad1991@gmail.com>
2015-09-25 02:24:44 -0700

    .

--- main_svm_test_verification_set.py
+++ main_svm_test_verification_set.py
@@ -7,15 +7,18 @@ import os
 
 def main(config):
     verification_directory = '/home/rna/RNA/VerificationSet'
+    verification_results = '/home/rna/RNA/VerificationResult'
     structures = os.listdir(verification_directory)
     for structure in structures:
         full_path = os.path.join(verification_directory, structure)
+
         t = load_template(full_path)
         sequence = str(t.get_sequence())
         model_path = 'model_'+structure
+        output_path = os.path.join(verification_results, model_path)
         print structure, sequence
         try:
-            main_build_model(sequence, config.get_svm_file(), model_path, config.get_template_directory())
+            main_build_model(sequence, config.get_svm_file(), output_path, config.get_template_directory())
         except:
             print "ERROR: ", structure
 
