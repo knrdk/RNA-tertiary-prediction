bc60b2a3460dcdf81be7ef48c4e99ca0ed8867c0
knrdk <konrad1991@gmail.com>
2015-09-27 05:55:52 -0700

    .

--- main_training_delete_redundant.py
+++ main_training_delete_redundant.py
@@ -2,8 +2,10 @@ __author__ = 'Konrad Kopciuch'
 
 
 from Repository.MongoTrainingTemplateRepository import MongoTrainingTemplateRepository
+from Repository.MongoTemplateRepository import MongoTemplateRepository
 from Config import Config
 from Training.DeleteTrainingTemplate import delete_template, delete_unused_template_files
+
 
 def main_delete_redundant(training_template_directory):
     '''
@@ -27,7 +29,21 @@ def main_delete_redundant(training_template_directory):
     delete_unused_template_files(repo, training_template_directory)
 
 
+def delete_same_sequences_as_template(training_template_directory):
+    template_repo = MongoTemplateRepository()
+    training_repo = MongoTrainingTemplateRepository()
+
+    sequences = map(lambda x: x[1], template_repo.get_all_unmodified_sequences())
+    for sequence in sequences:
+        for db_id in training_repo.get_templates_for_sequence(sequence):
+            print str(db_id)
+            delete_template(training_repo, db_id)
+
+    delete_unused_template_files(training_repo, training_template_directory)
+
+
 if __name__ == "__main__":
     config = Config('config.ini')
     training_template_directory = config.get_training_set_directory()
     main_delete_redundant(training_template_directory)
+    delete_same_sequences_as_template(training_template_directory)
